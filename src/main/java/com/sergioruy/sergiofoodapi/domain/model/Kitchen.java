6564e7be79ed605b8277de5b4e3ee996cd7fbd85
SergioRuyDev <sergioruyenator@gmail.com>
2021-05-09 16:19:27 +0400

    Database strategy added.

--- src/main/java/com/sergioruy/sergiofoodapi/domain/model/Kitchen.java
+++ src/main/java/com/sergioruy/sergiofoodapi/domain/model/Kitchen.java
@@ -1,6 +1,8 @@
 package com.sergioruy.sergiofoodapi.domain.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
@@ -8,6 +10,7 @@ import java.util.Objects;
 public class Kitchen {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
