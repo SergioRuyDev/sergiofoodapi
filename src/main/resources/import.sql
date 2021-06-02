f18c57c7e4a7d156233588841abb453e8a6bb44b
SergioRuyDev <sergioruyenator@gmail.com>
2021-06-02 16:12:34 +0400

    Relation Many to One added.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 insert into kitchen (name) values ('Thai');
 insert into kitchen (name) values ('Indian');
 
-insert into restaurant (names, tax_delivery) values ('Thai Gourmet', 10);
-insert into restaurant (names, tax_delivery) values ('Thai Delivery', 9.50);
-insert into restaurant (names, tax_delivery) values ('Tuk Tuk Indian Food', 15);
+insert into restaurant (name, tax_delivery) values ('Thai Gourmet', 10);
+insert into restaurant (name, tax_delivery) values ('Thai Delivery', 9.50);
+insert into restaurant (name, tax_delivery) values ('Tuk Tuk Indian Food', 15);
