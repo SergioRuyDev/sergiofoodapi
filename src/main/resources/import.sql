66d343819991798e921e5c4f9cc93c2f731793f5
SergioRuyDev <sergioruyenator@gmail.com>
2021-06-03 17:07:43 +0400

    More dummy data added.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,13 @@
 insert into kitchen (id, name) values (1, 'Thai');
 insert into kitchen (id, name) values (2, 'Indian');
+insert into kitchen (id, name) values (3, 'Brazilian');
+insert into kitchen (id, name) values (4, 'Japanese');
 
 insert into restaurant (name, tax_delivery, kitchen_id) values ('Thai Gourmet', 10, 1);
 insert into restaurant (name, tax_delivery, kitchen_id) values ('Thai Delivery', 9.50, 1);
 insert into restaurant (name, tax_delivery, kitchen_id) values ('Tuk Tuk Indian Food', 15, 2);
+insert into restaurant (name, tax_delivery, kitchen_id) values ('Carnival Brazilian', 25, 3);
+insert into restaurant (name, tax_delivery, kitchen_id) values ('Brazilian Beef', 20, 3);
+insert into restaurant (name, tax_delivery, kitchen_id) values ('Sumo Japan', 30, 4);
+insert into restaurant (name, tax_delivery, kitchen_id) values ('Sashimi Oss', 19, 4);
+
