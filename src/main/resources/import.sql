39023b0cd5e7ac5f88695492d0083a842eaf232e
SergioRuyDev <sergioruyenator@gmail.com>
2021-06-22 17:46:50 +0400

    SQL Data updated.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,3 +11,19 @@ insert into restaurant (name, tax_delivery, kitchen_id) values ('Brazilian Beef'
 insert into restaurant (name, tax_delivery, kitchen_id) values ('Sumo Japan', 30, 4);
 insert into restaurant (name, tax_delivery, kitchen_id) values ('Sashimi Oss', 19, 4);
 
+insert into estate (id, name) values (1, 'New York');
+insert into estate (id, name) values (2, 'Florida');
+insert into estate (id, name) values (3, 'California');
+
+insert into city (id, nome, estado_id) values (1, 'New York City', 1);
+insert into city (id, nome, estado_id) values (2, 'Western New York', 1);
+insert into city (id, nome, estado_id) values (3, 'Miami', 2);
+insert into city (id, nome, estado_id) values (4, 'Tampa', 2);
+insert into city (id, nome, estado_id) values (5, 'Los Angeles', 3);
+
+insert into payment_method (id, description) values (1, 'Credit Card');
+insert into payment_method (id, description) values (2, 'Debit Card');
+insert into payment_method (id, description) values (3, 'Cash');
+
+insert into permission (id, name, description) values (1, 'CONSULT_KITCHENS', 'Permit consult kitchens');
+insert into permission (id, name, description) values (2, 'EDIT_KITCHENS', 'Permit edit kitchens');
