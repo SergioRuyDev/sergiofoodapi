d3c9a4e9905bb398c44665c07cb427a264ed94c5
SergioRuyDev <sergioruyenator@gmail.com>
2021-06-24 17:54:40 +0400

    Media Type added.

--- src/main/java/com/sergioruy/sergiofoodapi/api/controller/KitchenController.java
+++ src/main/java/com/sergioruy/sergiofoodapi/api/controller/KitchenController.java
@@ -3,6 +3,7 @@ package com.sergioruy.sergiofoodapi.api.controller;
 import com.sergioruy.sergiofoodapi.domain.model.Kitchen;
 import com.sergioruy.sergiofoodapi.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/kitchens")
+@RequestMapping(value = "/kitchens", produces = MediaType.APPLICATION_JSON_VALUE)
 public class KitchenController {
 
     @Autowired
