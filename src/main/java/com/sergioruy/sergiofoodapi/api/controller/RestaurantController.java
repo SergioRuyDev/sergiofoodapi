d314835b72fd8694c2f585198d3363b559282395
SergioRuyDev <sergioruyenator@gmail.com>
2021-07-06 18:13:59 +0400

    Methods Get and FindById implemented for Restaurants.

--- src/main/java/com/sergioruy/sergiofoodapi/api/controller/RestaurantController.java
+++ src/main/java/com/sergioruy/sergiofoodapi/api/controller/RestaurantController.java
@@ -1,9 +1,15 @@
 package com.sergioruy.sergiofoodapi.api.controller;
 
+import com.sergioruy.sergiofoodapi.domain.model.Restaurant;
 import com.sergioruy.sergiofoodapi.domain.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/restaurants")
@@ -11,4 +17,20 @@ public class RestaurantController {
 
     @Autowired
     private RestaurantRepository restaurantRepository;
+
+    @GetMapping
+    public List<Restaurant> list() {
+        return restaurantRepository.list();
+    }
+
+    @GetMapping("/{restaurantId}")
+    public ResponseEntity<Restaurant> search(@PathVariable Long restaurantId) {
+        Restaurant restaurant = restaurantRepository.find(restaurantId);
+
+        if (restaurant != null) {
+            return ResponseEntity.ok(restaurant);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
