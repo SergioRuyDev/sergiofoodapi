8a54e9126f32c1f78880cb79bee95a12fc586c82
SergioRuyDev <sergioruyenator@gmail.com>
2021-07-07 17:51:39 +0400

    Restaurants resources in progress.

--- src/main/java/com/sergioruy/sergiofoodapi/api/controller/RestaurantController.java
+++ src/main/java/com/sergioruy/sergiofoodapi/api/controller/RestaurantController.java
@@ -4,6 +4,7 @@ import com.sergioruy.sergiofoodapi.domain.exception.EntityNotFoundException;
 import com.sergioruy.sergiofoodapi.domain.model.Restaurant;
 import com.sergioruy.sergiofoodapi.domain.repository.RestaurantRepository;
 import com.sergioruy.sergiofoodapi.domain.service.RegisterRestaurantService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,5 +46,23 @@ public class RestaurantController {
         } catch (EntityNotFoundException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @PutMapping("/{restaurantId}")
+    public ResponseEntity<?> update(@PathVariable Long restaurantId, @RequestBody Restaurant restaurant) {
+        try {
+            Restaurant currentRestaurant = restaurantRepository.find(restaurantId);
+
+            if (currentRestaurant != null) {
+                BeanUtils.copyProperties(restaurant, currentRestaurant, "id");
+
+                currentRestaurant = registerRestaurant.save(currentRestaurant);
+                return ResponseEntity.ok(currentRestaurant);
+            }
+            return ResponseEntity.notFound().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 }
