7dc1b44b9206d9cfac03ddeb7b76bfc703d99401
SergioRuyDev <sergioruyenator@gmail.com>
2021-07-10 17:59:39 +0400

    Refactor to Spring Data JPA Started.

--- src/main/java/com/sergioruy/sergiofoodapi/domain/repository/KitchenRepository.java
+++ src/main/java/com/sergioruy/sergiofoodapi/domain/repository/KitchenRepository.java
@@ -1,14 +1,13 @@
 package com.sergioruy.sergiofoodapi.domain.repository;
 
 import com.sergioruy.sergiofoodapi.domain.model.Kitchen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface KitchenRepository {
+@Repository
+public interface KitchenRepository extends JpaRepository<Kitchen, Long> {
 
-    List<Kitchen> list();
-    List<Kitchen> consultByName(String name);
-    Kitchen find(Long id);
-    Kitchen save(Kitchen kitchen);
-    void remove(Long id);
+//    List<Kitchen> consultByName(String name);
 }
