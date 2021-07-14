54d674096c4399ed14fcd38802e8e8a26fa494f1
SergioRuyDev <sergioruyenator@gmail.com>
2021-07-14 16:03:44 +0400

    Testing Jpa consults.

--- src/main/java/com/sergioruy/sergiofoodapi/api/controller/TestController.java
+++ src/main/java/com/sergioruy/sergiofoodapi/api/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.sergioruy.sergiofoodapi.api.controller;
 
 import com.sergioruy.sergiofoodapi.domain.model.Kitchen;
+import com.sergioruy.sergiofoodapi.domain.model.Restaurant;
 import com.sergioruy.sergiofoodapi.domain.repository.KitchenRepository;
+import com.sergioruy.sergiofoodapi.domain.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +20,9 @@ public class TestController {
     @Autowired
     private KitchenRepository kitchenRepository;
 
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
     @GetMapping("/kitchens/by-name")
     public List<Kitchen> allKitchenByName(String name) {
         return kitchenRepository.findAllByName(name);
@@ -25,5 +31,14 @@ public class TestController {
     @GetMapping("/kitchens/unique-by-name")
     public Optional<Kitchen> kitchenByName(String name) {
         return kitchenRepository.findByName(name);
+    }
+    @GetMapping("/restaurants/by-tax-delivery")
+    public List<Restaurant> restaurantsByTaxDelivery(BigDecimal initialTax, BigDecimal finalTax) {
+        return restaurantRepository.findByTaxDeliveryBetween(initialTax, finalTax);
+    }
+
+    @GetMapping("/restaurants/by-name")
+    public List<Restaurant> restaurantsByTaxDelivery(String name, Long kitchenId) {
+        return restaurantRepository.findByNameContainingAndKitchenId(name, kitchenId);
     }
 }
--- src/main/java/com/sergioruy/sergiofoodapi/domain/repository/RestaurantRepository.java
+++ src/main/java/com/sergioruy/sergiofoodapi/domain/repository/RestaurantRepository.java
@@ -4,7 +4,13 @@ import com.sergioruy.sergiofoodapi.domain.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
+    List<Restaurant> findByTaxDeliveryBetween(BigDecimal initialTax, BigDecimal finalTax);
+
+    List<Restaurant> findByNameContainingAndKitchenId(String name, Long kitchen);
 }
