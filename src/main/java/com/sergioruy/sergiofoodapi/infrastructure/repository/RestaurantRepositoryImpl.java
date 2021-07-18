511dd452eb15141527052234499fc58536b2b25d
SergioRuyDev <sergioruyenator@gmail.com>
2021-07-18 16:15:51 +0400

    Testing Jpa consults.

--- src/main/java/com/sergioruy/sergiofoodapi/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/sergioruy/sergiofoodapi/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -3,10 +3,13 @@ package com.sergioruy.sergiofoodapi.infrastructure.repository;
 import com.sergioruy.sergiofoodapi.domain.model.Restaurant;
 import com.sergioruy.sergiofoodapi.domain.repository.RestaurantRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -17,12 +20,29 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
 
     @Override
     public List<Restaurant> find(String name, BigDecimal taxDeliveryInitial, BigDecimal taxDeliveryFinal) {
-        var jpql = "from Restaurant where name like :name " + "and taxDelivery between :taxInitial and :taxFinal";
+        var jpql = new StringBuilder();
+        jpql.append("from Restaurant where 0 = 0 ");
 
-        return manager.createQuery(jpql, Restaurant.class)
-                .setParameter("name", "%" + name + "%")
-                .setParameter("taxInitial", taxDeliveryInitial)
-                .setParameter("taxFinal", taxDeliveryFinal)
-                .getResultList();
+        var parameters = new HashMap<String, Object>();
+
+        if (StringUtils.hasLength(name)) {
+            jpql.append("and name like :name ");
+            parameters.put("name", "%" + name + "%");
+        }
+
+        if (taxDeliveryInitial != null) {
+            jpql.append("and taxDelivery >= :taxInitial ");
+            parameters.put("taxInitial", taxDeliveryInitial);
+        }
+
+        if (taxDeliveryFinal != null) {
+            jpql.append("and taxDelivery <= :taxFinal ");
+            parameters.put("taxFinal", taxDeliveryFinal);
+        }
+
+        TypedQuery<Restaurant> query = manager.createQuery(jpql.toString(), Restaurant.class);
+
+        parameters.forEach((key, value) -> query.setParameter(key, value));
+        return query.getResultList();
     }
 }
