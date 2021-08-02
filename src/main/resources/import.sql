fcd15c03228eff4ba66eea10afa83dda0864ec13
SergioRuyDev <sergioruyenator@gmail.com>
2021-08-02 18:33:02 +0400

    Refactor sql file.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,14 +3,6 @@ insert into kitchen (id, name) values (2, 'Indian');
 insert into kitchen (id, name) values (3, 'Brazilian');
 insert into kitchen (id, name) values (4, 'Japanese');
 
-insert into restaurant (name, tax_delivery, kitchen_id) values ('Thai Gourmet', 10, 1);
-insert into restaurant (name, tax_delivery, kitchen_id) values ('Thai Delivery', 9.50, 1);
-insert into restaurant (name, tax_delivery, kitchen_id) values ('Tuk Tuk Indian Food', 15, 2);
-insert into restaurant (name, tax_delivery, kitchen_id) values ('Carnival Brazilian', 25, 3);
-insert into restaurant (name, tax_delivery, kitchen_id) values ('Brazilian Beef', 20, 3);
-insert into restaurant (name, tax_delivery, kitchen_id) values ('Sumo Japan', 30, 4);
-insert into restaurant (name, tax_delivery, kitchen_id) values ('Sashimi Oss', 19, 4);
-
 insert into state (id, name) values (1, 'New York');
 insert into state (id, name) values (2, 'Florida');
 insert into state (id, name) values (3, 'California');
@@ -20,6 +12,14 @@ insert into city (id, name, state_id) values (2, 'Western New York', 1);
 insert into city (id, name, state_id) values (3, 'Miami', 2);
 insert into city (id, name, state_id) values (4, 'Tampa', 2);
 insert into city (id, name, state_id) values (5, 'Los Angeles', 3);
+
+insert into restaurant (id, name, tax_delivery, kitchen_id, address_city_id, address_postalCode, address_street, address_number, address_district) values (1, 'Thai Gourmet', 10, 1, 1, '10292', '5TH Avenue', '1000', 'Yorkville');
+insert into restaurant (id, name, tax_delivery, kitchen_id) values (2, 'Thai Delivery', 9.50, 1);
+insert into restaurant (id, name, tax_delivery, kitchen_id) values (3, 'Tuk Tuk Indian Food', 15, 2);
+insert into restaurant (id, name, tax_delivery, kitchen_id) values (4, 'Carnival Brazilian', 25, 3);
+insert into restaurant (id, name, tax_delivery, kitchen_id) values (5, 'Brazilian Beef', 20, 3);
+insert into restaurant (id, name, tax_delivery, kitchen_id) values (6, 'Sumo Japan', 30, 4);
+insert into restaurant (id, name, tax_delivery, kitchen_id) values (7, 'Sashimi Oss', 19, 4);
 
 insert into payment_method (id, description) values (1, 'Credit Card');
 insert into payment_method (id, description) values (2, 'Debit Card');
