9801f933b58e910207a226e9bbc7aea1da2da589
SergioRuyDev <sergioruyenator@gmail.com>
2021-08-20 13:09:14 +0400

    Preparation for migration 004.

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,3 +1,38 @@
+create table payment_method (
+        id bigint not null auto_increment,
+        description varchar(60) not null,
+        primary key (id)
+) engine=InnoDB default charset=utf8mb4;
+
+create table group (
+        id bigint not null auto_increment,
+        name varchar(60) not null,
+        primary key (id)
+) engine=InnoDB default charset=utf8mb4;
+
+create table group_permission (
+        group_id bigint not null,
+        permission_id bigint not null
+) engine=InnoDB default charset=utf8mb4;
+
+create table permission (
+        id bigint not null auto_increment,
+        description varchar(255) not null,
+        name varchar(255) not null,
+        primary key (id)
+) engine=InnoDB default charset=utf8mb4;
+
+create table product (
+        id bigint not null auto_increment,
+        active bit not null,
+        description varchar(255) not null,
+        name varchar(255) not null,
+        price decimal(19,2) not null,
+        restaurant_id bigint not null,
+        primary key (id)
+) engine=InnoDB default charset=utf8mb4;
+
+
 
     create table city (
        id bigint not null auto_increment,
@@ -6,60 +41,27 @@
         primary key (id)
     ) engine=InnoDB
 
-    create table group (
-       id bigint not null auto_increment,
-        name varchar(255) not null,
-        primary key (id)
-    ) engine=InnoDB
-
-    create table group_permission (
-       group_id bigint not null,
-        permission_id bigint not null
-    ) engine=InnoDB
-
     create table kitchen (
        id bigint not null auto_increment,
         name varchar(255) not null,
         primary key (id)
     ) engine=InnoDB
 
-    create table payment_method (
-       id bigint not null auto_increment,
-        description varchar(255) not null,
-        primary key (id)
-    ) engine=InnoDB
-
-    create table permission (
-       id bigint not null auto_increment,
-        description varchar(255) not null,
-        name varchar(255) not null,
-        primary key (id)
-    ) engine=InnoDB
-
-    create table product (
-       id bigint not null auto_increment,
-        active bit not null,
-        description varchar(255) not null,
-        name varchar(255) not null,
-        price decimal(19,2) not null,
-        restaurant_id bigint not null,
-        primary key (id)
-    ) engine=InnoDB
-
     create table restaurant (
        id bigint not null auto_increment,
-        address_complement varchar(255),
-        address_district varchar(255),
-        address_number varchar(255),
-        address_postalcode varchar(255),
-        address_street varchar(255),
-        date_register datetime not null,
-        date_update datetime not null,
-        name varchar(255) not null,
-        tax_delivery decimal(19,2) not null,
-        address_city_id bigint,
-        kitchen_id bigint not null,
-        primary key (id)
+       kitchen_id bigint not null,
+       name varchar(255) not null,
+       tax_delivery decimal(19,2) not null,
+       date_update datetime not null,
+       date_register datetime not null,
+
+       address_city_id bigint,
+       address_postalcode varchar(255),
+       address_street varchar(255),
+       address_number varchar(255),
+       address_complement varchar(255),
+       address_district varchar(255),
+       primary key (id)
     ) engine=InnoDB
 
     create table restaurant_payment_method (
@@ -136,40 +138,7 @@
        add constraint FK1c1dsw3q36679vaiqwvtv36a6 
        foreign key (user_id) 
        references user (id)
-insert into kitchen (id, name) values (1, 'Thai')
-insert into kitchen (id, name) values (2, 'Indian')
-insert into kitchen (id, name) values (3, 'Brazilian')
-insert into kitchen (id, name) values (4, 'Japanese')
-insert into state (id, name) values (1, 'New York')
-insert into state (id, name) values (2, 'Florida')
-insert into state (id, name) values (3, 'California')
-insert into city (id, name, state_id) values (1, 'New York City', 1)
-insert into city (id, name, state_id) values (2, 'Western New York', 1)
-insert into city (id, name, state_id) values (3, 'Miami', 2)
-insert into city (id, name, state_id) values (4, 'Tampa', 2)
-insert into city (id, name, state_id) values (5, 'Los Angeles', 3)
-insert into restaurant (id, name, tax_delivery, kitchen_id, date_register, date_update, address_city_id, address_postalcode, address_street, address_number, address_district) values (1, 'Thai Gourmet', 10, 1, utc_timestamp, utc_timestamp, 1, '10292', '5TH Avenue', '1000', 'Yorkville')
-insert into restaurant (id, name, tax_delivery, kitchen_id, date_register, date_update) values (2, 'Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp)
-insert into restaurant (id, name, tax_delivery, kitchen_id, date_register, date_update) values (3, 'Tuk Tuk Indian Food', 15, 2, utc_timestamp, utc_timestamp)
-insert into restaurant (id, name, tax_delivery, kitchen_id, date_register, date_update) values (4, 'Carnival Brazilian', 25, 3, utc_timestamp, utc_timestamp)
-insert into restaurant (id, name, tax_delivery, kitchen_id, date_register, date_update) values (5, 'Brazilian Beef', 20, 3, utc_timestamp, utc_timestamp)
-insert into restaurant (id, name, tax_delivery, kitchen_id, date_register, date_update) values (6, 'Sumo Japan', 30, 4, utc_timestamp, utc_timestamp)
-insert into restaurant (id, name, tax_delivery, kitchen_id, date_register, date_update) values (7, 'Sashimi Oss', 19, 4, utc_timestamp, utc_timestamp)
-insert into payment_method (id, description) values (1, 'Credit Card')
-insert into payment_method (id, description) values (2, 'Debit Card')
-insert into payment_method (id, description) values (3, 'Cash')
-insert into permission (id, name, description) values (1, 'CONSULT_KITCHENS', 'Permit consult kitchens')
-insert into permission (id, name, description) values (2, 'EDIT_KITCHENS', 'Permit edit kitchens')
-insert into restaurant_payment_method (restaurant_id, payment_method_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3)
-insert into product (name, description, price, active, restaurant_id) values ('Pork with barbecue sauce', 'Delicious pork meat in especial sauce', 78.90, 1, 1)
-insert into product (name, description, price, active, restaurant_id) values ('Shrimp thai', '16 big shrimps in the spicy sauce', 110, 1, 1)
-insert into product (name, description, price, active, restaurant_id) values ('Hot salad will grilled beef', 'Green salad with thin slices cuts of select premium Angus meat', 87.20, 1, 2)
-insert into product (name, description, price, active, restaurant_id) values ('Garlic Naan', 'Traditional indian bread with garlic on top', 21, 1, 3)
-insert into product (name, description, price, active, restaurant_id) values ('Murg Curry', 'Chicken cubes prepared with especial curry sauce', 43, 1, 3)
-insert into product (name, description, price, active, restaurant_id) values ('Beef Anchor', 'Big and soft cut, prepared with selected seasoning', 79, 1, 4)
-insert into product (name, description, price, active, restaurant_id) values ('T-Bone', 'Classic australian cut, served with our especial barbecue sauce', 89, 1, 4)
-insert into product (name, description, price, active, restaurant_id) values ('Sandwich X-Tudo', 'Brazilian sandwich with cheese, hamburger, bacon, egg, salad and especial mayo', 19, 1, 5)
-insert into product (name, description, price, active, restaurant_id) values ('Barbecue Stick', 'came with cassava flour and vinegar sauce', 8, 1, 6)
+
 
     create table city (
        id bigint not null auto_increment,
