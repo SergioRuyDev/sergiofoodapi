986be3b305762fd6794d15a690dc7d19fe4c9061
SergioRuyDev <sergioruyenator@gmail.com>
2021-08-22 17:41:35 +0400

    New entities in progress.

---
+++ src/main/java/com/sergioruy/sergiofoodapi/domain/model/ItemOrdered.java
@@ -0,0 +1,38 @@
+package com.sergioruy.sergiofoodapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class ItemOrdered {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = true)
+    private String observations;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
--- src/main/java/com/sergioruy/sergiofoodapi/domain/model/Order.java
+++ src/main/java/com/sergioruy/sergiofoodapi/domain/model/Order.java
@@ -6,6 +6,8 @@ import lombok.EqualsAndHashCode;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
@@ -29,8 +31,16 @@ public class Order {
     @Column(nullable = true)
     private LocalDateTime createDate;
 
+    @Column(nullable = true)
     private LocalDateTime confirmDate;
+
+    @Column(nullable = true)
     private LocalDateTime cancelDate;
+
+    @Column(nullable = true)
     private LocalDateTime DeliveryDate;
+
+    @OneToMany(mappedBy = "itemOrdered")
+    private List<ItemOrdered> itemOrdereds = new ArrayList<>();
 
 }
