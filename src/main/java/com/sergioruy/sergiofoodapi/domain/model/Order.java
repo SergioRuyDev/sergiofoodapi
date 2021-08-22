a14bc77265ba37730f7807f4cd2f7ef11a924d80
SergioRuyDev <sergioruyenator@gmail.com>
2021-08-22 17:22:43 +0400

    New entities in progress.

--- src/main/java/com/sergioruy/sergiofoodapi/domain/model/Order.java
+++ src/main/java/com/sergioruy/sergiofoodapi/domain/model/Order.java
@@ -1,24 +1,34 @@
 package com.sergioruy.sergiofoodapi.domain.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
 @Entity
 public class Order {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private BigDecimal subtotal;
+
+    @Column(nullable = false)
     private BigDecimal taxDelivery;
+
+    @Column(nullable = false)
     private BigDecimal totalAmount;
 
+    @Column(nullable = true)
     private LocalDateTime createDate;
+
     private LocalDateTime confirmDate;
     private LocalDateTime cancelDate;
     private LocalDateTime DeliveryDate;
