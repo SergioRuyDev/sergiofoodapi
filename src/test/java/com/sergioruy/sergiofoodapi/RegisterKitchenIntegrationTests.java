676b4548ba7e8038e1299a4a9a5168f2c7567b99
SergioRuyDev <sergioruyenator@gmail.com>
2021-11-06 11:36:28 +0400

    Integration tests in progress.

---
+++ src/test/java/com/sergioruy/sergiofoodapi/RegisterKitchenIntegrationTests.java
@@ -0,0 +1,46 @@
+package com.sergioruy.sergiofoodapi;
+
+import com.sergioruy.sergiofoodapi.domain.model.Kitchen;
+import com.sergioruy.sergiofoodapi.domain.service.RegisterKitchenService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class RegisterKitchenIntegrationTests {
+
+    @Autowired
+    private RegisterKitchenService registerKitchen;
+
+    @Test
+    public void testRegisterKitchenNoName() {
+//        //scenario
+//        Kitchen newKitchen = new Kitchen();;
+//        newKitchen.setName("Chinese");
+//
+//        //action
+//        newKitchen = registerKitchen.save(newKitchen);
+//
+//        //validation
+//        assertThat(newKitchen).isNotNull();
+//        assertThat(newKitchen.getId()).isNotNull();
+//
+        Kitchen newKitchen = new Kitchen();
+        newKitchen.setName(null);
+
+        ConstraintViolationException errorExpected =
+                Assertions.assertThrows(ConstraintViolationException.class, () -> {
+                   registerKitchen.save(newKitchen);
+                });
+
+        assertThat(errorExpected).isNotNull();
+    }
+}
