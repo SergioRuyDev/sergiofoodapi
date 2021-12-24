4ed54135eeea594fdffbe8f274ac4d2153beceef
SergioRuyDev <sergioruyenator@gmail.com>
2021-12-24 11:19:02 +0400

    Tests of api in progress.

--- src/test/java/com/sergioruy/sergiofoodapi/RegisterKitchenIT.java
+++ src/test/java/com/sergioruy/sergiofoodapi/RegisterKitchenIT.java
@@ -3,6 +3,7 @@ package com.sergioruy.sergiofoodapi;
 import com.sergioruy.sergiofoodapi.domain.model.Kitchen;
 import com.sergioruy.sergiofoodapi.domain.repository.KitchenRepository;
 import com.sergioruy.sergiofoodapi.util.DatabaseCleaner;
+import com.sergioruy.sergiofoodapi.util.ResourceUtils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,13 +17,14 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource("/application-test.properties")
 class RegisterKitchenIT {
+
+    private static final int KITCHEN_ID_NONEXISTENT = 100;
 
     @LocalServerPort
     private int port;
@@ -33,15 +35,106 @@ class RegisterKitchenIT {
     @Autowired
     private KitchenRepository kitchenRepository;
 
+    private Kitchen americanKitchen;
+    private int amountRegisteredKitchens;
+    private String jsonCorrectChineseKitchen;
+
     @BeforeEach
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/kitchens";
 
+        jsonCorrectChineseKitchen = ResourceUtils.getContentFromResource(
+                "/json/correct/chinese-kitchen.json");
+
         databaseCleaner.clearTables();
         prepareData();
     }
+
+
+
+    @Test
+    public void shouldReturnStatus_200_WhenConsultKitchens() {
+        given()
+                .accept(ContentType.JSON)
+        .when()
+                .get()
+        .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void shouldReturnCorrectAmountOfKitchens_WhenConsultKitchens() {
+        given()
+                .accept(ContentType.JSON)
+        .when()
+                .get()
+        .then()
+                .body("", hasSize(amountRegisteredKitchens));
+
+    }
+
+    @Test
+    public void shouldReturnStatus201_WhenRegisterKitchen() {
+        given()
+                .body(jsonCorrectChineseKitchen)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+        .when()
+                .post()
+        .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+//        Kitchen kitchen = new Kitchen();
+//        kitchen.setName("Chinese");
+//
+//        given()
+//                .body(kitchen)
+//                .contentType(ContentType.JSON)
+//                .accept(ContentType.JSON)
+//        .when()
+//                .post()
+//        .then()
+//                .statusCode(HttpStatus.CREATED.value());
+//    }
+
+    @Test
+    public void shouldReturnCorrectResponseStatus_WhenConsultExistedKitchen() {
+        given()
+                .pathParams("kitchenId", americanKitchen.getId())
+                .accept(ContentType.JSON)
+        .when()
+                .get("/{kitchenId}")
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("name", equalTo(americanKitchen.getName()));
+
+    }
+
+    @Test
+    public void shouldReturnStatus404_WhenConsultKitchenNonExistent() {
+        given()
+                .pathParams("kitchenId", KITCHEN_ID_NONEXISTENT)
+                .accept(ContentType.JSON)
+        .when()
+                .get("/{kitchenId}")
+        .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    private void prepareData() {
+        Kitchen thayKitchen = new Kitchen();
+        thayKitchen.setName("Thai");
+        kitchenRepository.save(thayKitchen);
+
+        Kitchen americanKitchen = new Kitchen();
+        americanKitchen.setName("American");
+        kitchenRepository.save(americanKitchen);
+
+
+    }
+}
 
 //    @Autowired
 //    private RegisterKitchenService registerKitchen;
@@ -90,51 +183,3 @@ class RegisterKitchenIT {
 //                    registerKitchen.delete(100L);
 //                });
 //    }
-
-    @Test
-    public void shouldReturnStatus_200_WhenConsultKitchens() {
-        given()
-                .accept(ContentType.JSON)
-        .when()
-                .get()
-        .then()
-                .statusCode(HttpStatus.OK.value());
-    }
-
-    @Test
-    public void shouldHave2Kitchens_WhenConsultKitchens() {
-        given()
-                .accept(ContentType.JSON)
-        .when()
-                .get()
-        .then()
-                .body("", hasSize(2))
-                .body("name", hasItems("Thai", "Indian"));
-
-    }
-
-    @Test
-    public void shouldReturnStatus201_WhenRegisterKitchen() {
-        Kitchen kitchen = new Kitchen();
-        kitchen.setName("Chinese");
-
-        given()
-                .body(kitchen)
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-        .when()
-                .post()
-        .then()
-                .statusCode(HttpStatus.CREATED.value());
-    }
-
-    private void prepareData() {
-        Kitchen kitchen1 = new Kitchen();
-        kitchen1.setName("Thai");
-        kitchenRepository.save(kitchen1);
-
-        Kitchen kitchen2 = new Kitchen();
-        kitchen2.setName("American");
-        kitchenRepository.save(kitchen2);
-    }
-}
