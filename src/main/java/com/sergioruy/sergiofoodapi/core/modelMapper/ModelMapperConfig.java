e09f7c8ed15a6967a7e148f3a8efc69d1e464549
SergioRuyDev <sergioruyenator@gmail.com>
2022-01-01 20:46:27 +0400

    Model Mapper added.

--- src/main/java/com/sergioruy/sergiofoodapi/core/modelMapper/ModelMapperConfig.java
+++ src/main/java/com/sergioruy/sergiofoodapi/core/modelMapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.sergioruy.sergiofoodapi.core.modelMapper;
 
+import com.sergioruy.sergiofoodapi.api.model.RestaurantModel;
+import com.sergioruy.sergiofoodapi.domain.model.Restaurant;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,4 +13,13 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    //Customize the properties
+//    public ModelMapper modelMapper() {
+//        var modelMapper = new ModelMapper();
+//
+//        modelMapper.createTypeMap(Restaurant.class, RestaurantModel.class)
+//                .addMapping(Restaurant::getTaxDelivery, RestaurantModel::setPriceDelivery);
+//        return modelMapper;
+//    }
 }
