b50d1fc57b82a2cc648128544bd2a2fc4c59b060
SergioRuyDev <sergioruyenator@gmail.com>
2022-02-24 10:36:52 -0300

    Payment Method layer done.

--- src/main/java/com/sergioruy/sergiofoodapi/api/assembler/PaymentMethodModelAssembler.java
+++ src/main/java/com/sergioruy/sergiofoodapi/api/assembler/PaymentMethodModelAssembler.java
@@ -3,6 +3,7 @@ package com.sergioruy.sergiofoodapi.api.assembler;
 import com.sergioruy.sergiofoodapi.api.model.PaymentMethodModel;
 import com.sergioruy.sergiofoodapi.domain.model.PaymentMethod;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.stream.Collectors;
 @Component
 public class PaymentMethodModelAssembler {
 
+    @Autowired
     private ModelMapper modelMapper;
 
     public PaymentMethodModel toModel(PaymentMethod paymentMethod) {
--- src/main/java/com/sergioruy/sergiofoodapi/api/controller/PaymentMethodController.java
+++ src/main/java/com/sergioruy/sergiofoodapi/api/controller/PaymentMethodController.java
@@ -3,15 +3,15 @@ package com.sergioruy.sergiofoodapi.api.controller;
 import com.sergioruy.sergiofoodapi.api.assembler.PaymentMethodInputDisassembler;
 import com.sergioruy.sergiofoodapi.api.assembler.PaymentMethodModelAssembler;
 import com.sergioruy.sergiofoodapi.api.model.PaymentMethodModel;
+import com.sergioruy.sergiofoodapi.api.model.input.PaymentMethodInput;
 import com.sergioruy.sergiofoodapi.domain.model.PaymentMethod;
 import com.sergioruy.sergiofoodapi.domain.repository.PaymentMethodRepository;
 import com.sergioruy.sergiofoodapi.domain.service.RegisterPaymentMethodService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -43,5 +43,27 @@ public class PaymentMethodController {
         return paymentMethodModelAssembler.toModel(paymentMethod);
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PaymentMethodModel add(@RequestBody @Valid PaymentMethodInput paymentMethodInput) {
+            PaymentMethod paymentMethod = paymentMethodInputDisassembler.toDomainObject(paymentMethodInput);
+
+            return paymentMethodModelAssembler.toModel(registerPaymentMethodService.save(paymentMethod));
+    }
+
+    @PutMapping("/{paymentMethodId}")
+    public PaymentMethodModel update(@PathVariable Long paymentMethodId, @RequestBody PaymentMethodInput paymentMethodInput) {
+        PaymentMethod currentPayment = registerPaymentMethodService.findOrFail(paymentMethodId);
+
+        paymentMethodInputDisassembler.copyToDomainObject(paymentMethodInput, currentPayment);
+
+        return paymentMethodModelAssembler.toModel(registerPaymentMethodService.save(currentPayment));
+    }
+
+    @DeleteMapping("/{paymentMethodId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remove(@PathVariable Long paymentMethodId) {
+        registerPaymentMethodService.delete(paymentMethodId);
+    }
 
 }
