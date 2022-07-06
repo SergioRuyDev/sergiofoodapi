2b63dba84a48edb16486ef227b5cc027f5bc3f3f
SergioRuyDev <sergioruyenator@gmail.com>
2022-07-06 11:36:41 -0300

    Endpoint of Product in progress.

--- src/main/java/com/sergioruy/sergiofoodapi/api/assembler/ProductModelAssembler.java
+++ src/main/java/com/sergioruy/sergiofoodapi/api/assembler/ProductModelAssembler.java
@@ -4,11 +4,13 @@ import com.sergioruy.sergiofoodapi.api.model.ProductModel;
 import com.sergioruy.sergiofoodapi.domain.model.Product;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class ProductModelAssembler {
 
     @Autowired
--- src/main/java/com/sergioruy/sergiofoodapi/api/controller/ProductController.java
+++ src/main/java/com/sergioruy/sergiofoodapi/api/controller/ProductController.java
@@ -1,9 +1,31 @@
 package com.sergioruy.sergiofoodapi.api.controller;
 
+import com.sergioruy.sergiofoodapi.api.assembler.ProductModelAssembler;
+import com.sergioruy.sergiofoodapi.api.model.ProductModel;
+import com.sergioruy.sergiofoodapi.domain.model.Product;
+import com.sergioruy.sergiofoodapi.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductModelAssembler productModelAssembler;
+
+    @GetMapping
+    public List<ProductModel> list() {
+        List<Product> allProducts = productRepository.findAll();
+
+        return productModelAssembler.toCollection(allProducts);
+    }
 }
