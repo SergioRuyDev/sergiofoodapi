4f8b54065dc255ff830c16e6af00dc31aa10c78d
SergioRuyDev <sergioruyenator@gmail.com>
2022-07-06 14:28:47 -0300

    Endpoint of Product in progress.

--- src/main/java/com/sergioruy/sergiofoodapi/api/controller/ProductController.java
+++ src/main/java/com/sergioruy/sergiofoodapi/api/controller/ProductController.java
@@ -3,7 +3,10 @@ package com.sergioruy.sergiofoodapi.api.controller;
 import com.sergioruy.sergiofoodapi.api.assembler.ProductModelAssembler;
 import com.sergioruy.sergiofoodapi.api.model.ProductModel;
 import com.sergioruy.sergiofoodapi.domain.model.Product;
+import com.sergioruy.sergiofoodapi.domain.model.Restaurant;
 import com.sergioruy.sergiofoodapi.domain.repository.ProductRepository;
+import com.sergioruy.sergiofoodapi.domain.service.RegisterProductService;
+import com.sergioruy.sergiofoodapi.domain.service.RegisterRestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/restaurants/{restaurantId}/products")
 public class ProductController {
+
+    @Autowired
+    private RegisterRestaurantService restaurantService;
+
+    @Autowired
+    private RegisterProductService productService;
 
     @Autowired
     private ProductRepository productRepository;
@@ -23,9 +32,18 @@ public class ProductController {
     private ProductModelAssembler productModelAssembler;
 
     @GetMapping
-    public List<ProductModel> list() {
-        List<Product> allProducts = productRepository.findAll();
+    public List<ProductModel> list(@PathVariable Long restaurantId) {
+        Restaurant restaurant = restaurantService.findOrFail(restaurantId);
+
+        List<Product> allProducts = productRepository.findByRestaurant(restaurant);
 
         return productModelAssembler.toCollection(allProducts);
+    }
+
+    @GetMapping("/{productId}")
+    public ProductModel search(@PathVariable Long restaurantId, @PathVariable Long productId) {
+        Product product = productService.findOrFail(restaurantId, productId);
+
+        return productModelAssembler.toModel(product);
     }
 }
