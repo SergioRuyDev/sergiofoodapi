08c3c3158c6c99a41023e89d4616e7da83025d59
sergioruydev <sergioruyenator@gmail.com>
2023-02-16 17:16:30 -0300

    Statics begin.

--- src/main/java/com/sergioruy/sergiofoodapi/domain/model/dto/DailySale.java
+++ src/main/java/com/sergioruy/sergiofoodapi/domain/model/dto/DailySale.java
@@ -1,14 +1,18 @@
 package com.sergioruy.sergiofoodapi.domain.model.dto;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.Date;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
 public class DailySale {
 
-    private LocalDate date;
+    private Date date;
     private Long totalSales;
     private BigDecimal totalInvoiced;
 }
--- src/main/java/com/sergioruy/sergiofoodapi/infrastructure/service/SaleQueryServiceImpl.java
+++ src/main/java/com/sergioruy/sergiofoodapi/infrastructure/service/SaleQueryServiceImpl.java
@@ -1,15 +1,38 @@
 package com.sergioruy.sergiofoodapi.infrastructure.service;
 
+import com.sergioruy.sergiofoodapi.domain.model.Order;
 import com.sergioruy.sergiofoodapi.domain.model.dto.DailySale;
 import com.sergioruy.sergiofoodapi.domain.model.filter.DailySaleFilter;
 import com.sergioruy.sergiofoodapi.domain.service.SaleQueryService;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
 import java.util.List;
 
+@Repository
 public class SaleQueryServiceImpl implements SaleQueryService {
+
+    @PersistenceContext
+    private EntityManager manager;
 
     @Override
     public List<DailySale> consultDiarySales(DailySaleFilter filter) {
-        return null;
+        var builder = manager.getCriteriaBuilder();
+        var query = builder.createQuery(DailySale.class);
+        var root = query.from(Order.class);
+
+        var functionCreatedDate = builder.function("date", Date.class, root.get("createdDate"));
+
+        var selection = builder.construct(DailySale.class,
+                functionCreatedDate,
+                builder.count(root.get("id")),
+                builder.sum(root.get("totalAmount")));
+
+        query.select(selection);
+        query.groupBy(functionCreatedDate);
+
+        return manager.createQuery(query).getResultList();
     }
 }
